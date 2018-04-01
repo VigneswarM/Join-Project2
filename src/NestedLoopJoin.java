b8f9ad614d0287390229dae8f3205a597586c044
vaishnavi <whyshu@gmail.com>
2018-04-01 01:57:43 -0400

    Nested Loop join working completely with Tuple count 1100466,ram usage 1.1MB and time of 12 minutes

--- src/NestedLoopJoin.java
+++ src/NestedLoopJoin.java
@@ -10,10 +10,24 @@ public class NestedLoopJoin {
 	File relation1,relation2;
     BufferedWriter bw;
 
-    int lineCounter1 = 0,lineCounter2 = 0;
+    int lineCounter1 = 0,lineCounter2 = 0,dataSizeToBeLoaded=0;
     HashMap<Integer,String> relation1Lines = new HashMap<>();
     ArrayList<String> relation2Lines=new ArrayList<String>();
 
+    private int calcSizeOfDataTobeLoaded(){
+    	File smallRelation;
+    	int availableRam=(int)Runtime.getRuntime().freeMemory();
+    	if(relation1.length()<relation2.length()){
+    		smallRelation=relation1;
+    	}else{
+    		smallRelation=relation2;
+    	}
+    	if(smallRelation.length()<availableRam/2){
+    		return (int)smallRelation.length()/Constants.BLOCK_SIZE;
+    	}else{
+    		return (int)smallRelation.length()/(2*Constants.BLOCK_SIZE);
+    	}
+    }
     public NestedLoopJoin(){
         try {
             relation1 =new File(Constants.DATA_DIR+"JoinT1.txt");
@@ -24,6 +38,8 @@ public class NestedLoopJoin {
             FileWriter writer = new FileWriter(Constants.DATA_DIR + "output.txt", true);
             bw = new BufferedWriter(writer);
             
+            dataSizeToBeLoaded=calcSizeOfDataTobeLoaded();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -31,7 +47,7 @@ public class NestedLoopJoin {
 
     private ArrayList<String> loadT2Data(){
         int count=lineCounter2+Constants.TUPLE_COUNT2;
-        System.out.println("Loading R2 :: "+count);
+        //System.out.println("Loading R2 :: "+count);
         while ((lineCounter2 < count) && input2.hasNextLine()) {
         	relation2Lines.add(input2.nextLine());
             lineCounter2++;
@@ -40,8 +56,9 @@ public class NestedLoopJoin {
     }
     
     private void loadT1Data(){
-        int count=lineCounter1+(Constants.BLOCK_COUNT1/2)*Constants.TUPLE_COUNT1;
-        System.out.println("Loading R1 :: "+count);
+    	//System.out.println(lineCounter1+" "+dataSizeToBeLoaded+" "+Constants.TUPLE_COUNT1);
+        int count=lineCounter1+((dataSizeToBeLoaded/2)*Constants.TUPLE_COUNT1);
+        //System.out.println("Loading R1 :: "+count);
     	while (lineCounter1 < count&& input1.hasNextLine()) {
         	String curr=input1.nextLine();
             relation1Lines.put(new Student(curr).ID,curr);
